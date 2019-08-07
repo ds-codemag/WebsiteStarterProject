e10738e6e36c9e0ebb970b5a3a972031a723de91
ds-codemag <daniel.sobota@codemag.pl>
2019-08-07 20:45:02 +0200

    Change code indentation, fix font and link styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,1 @@
 import '../style/stylesheet.scss';
-
-function component() {
-  let element = document.createElement('p');
-
-  element.innerText = 'Website Starter Project';
-
-  return element;
-}
-
-document.body.appendChild(component());
